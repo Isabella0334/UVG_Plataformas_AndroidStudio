2c692cedcad5ed8129e0a8536a2c8cd46c820163
Isabella0334 <isabellarecinos@gmail.com>
2024-08-06 23:23:51 -0600

    Main function and MyApp function

--- Laboratorio1/app/src/main/java/com/example/laboratorio1/MainActivity.kt
+++ Laboratorio1/app/src/main/java/com/example/laboratorio1/MainActivity.kt
@@ -3,45 +3,46 @@ package com.example.laboratorio1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.laboratorio1.ui.theme.Laboratorio1Theme
+
+data class Item(val name: String)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Laboratorio1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyApp() {
+    val items = listOf(
+        Item("Item 1"),
+        Item("Item 2"),
+        Item("Item 3")
     )
-}
+    var selectedItem = remember { mutableStateOf<Item?>(null) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratorio1Theme {
-        Greeting("Android")
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (selectedItem.value == null) {
+            Text("Select an item from the list")
+        } else {
+            Text("Selected: ${selectedItem.value?.name}")
+        }
     }
 }
